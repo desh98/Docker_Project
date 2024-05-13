ea75d87b894e65cdee8c16300203259013c84b69
desh98 <73052432+desh98@users.noreply.github.com>
2024-05-13 17:49:59 +0530

    Nodered => Encrypted, Flows storing automated

---
+++ nodered/settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2b$08$xWUc0Dla4RSD3yOONZafUuUpv/OmjStc8CP37MxHzVR9mxUAIGGki",
+        }]
+    },
+    
+};
